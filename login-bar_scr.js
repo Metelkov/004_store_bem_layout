8c87d0f78591cead55f1c4325007e0c94b45e877
metelkov <al.metelkov@gmail.com>
2021-01-17 17:23:39 +0300

    questchens

--- login-bar_scr.js
+++ login-bar_scr.js
@@ -1,7 +1,7 @@
 document.querySelector('.login-bar__exit-pic').addEventListener('click', function(event) {
     switch (event.target.className) {
     case 'login-bar__exit-pic':
-        debugger
+        //debugger
         alert("exiting");
         break;
     }
@@ -10,12 +10,19 @@ document.querySelector('.login-bar__exit-pic').addEventListener('click', functio
 document.querySelector('.login-bar__buttons-login').addEventListener('click', function(event) {
     switch (event.target.className) {
     case 'login-bar__buttons-login':
-        debugger
+        //debugger
         document.getElementById("dialog").showModal();
         break;
     }
     });
 
-
-
-
+    document.querySelector('.login-bar__buttons-close-form').addEventListener('click', function(event) {
+        switch (event.target.className) {
+        case 'login-bar__buttons-close-form':
+            //debugger
+            //document.getElementById("login-bar__buttons-close-form").modal.close();
+            // так тоже работает dialog.close();
+            dialog.close('dialog');
+            break;
+        }
+        });
